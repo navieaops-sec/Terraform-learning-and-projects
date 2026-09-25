96a391380930299d8ecd09c07af0ff5b7cd18753
navieaops-sec <navyakumar8121@gmail.com>
2026-09-25 17:07:38 +0530

    Add Terraform basics S3 project

---
+++ 01-terraform-basics/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.14.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_s3_bucket" "demo" {
+  bucket = "navyatesterbucket01-2026"
+}
